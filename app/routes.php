e10ff86efb2551164fb85abe9d1624db17734bdb
Jeff Mills <jeffrey.mills@outlook.com>
2014-05-23 17:15:29 -0500

    Config

--- app/routes.php
+++ app/routes.php
@@ -6,3 +6,8 @@ Route::get('/login','LoginController@index');
 Route::post('/login','LoginController@login');
 
 Route::get('/logout','LoginController@logout');
+
+Route::get('/users', function() {
+	$users = User::get();
+	return $users;
+});
