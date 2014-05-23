091a6272bb00687538c4cc8fb6ab0359e9bce724
Jeff Mills <jeffrey.mills@outlook.com>
2014-05-23 17:01:50 -0500

    config

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => $_ENV['database'],
-			'username'  => $_ENV['username'],
-			'password'  => $_ENV['password'],
+			'database'  => $_ENV['db'],
+			'username'  => $_ENV['db_username'],
+			'password'  => $_ENV['db_password'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
