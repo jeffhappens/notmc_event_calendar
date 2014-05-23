1fce6540e6625d7ca2bc3e3071dd06532e5e2814
Jeff Mills <jeffrey.mills@outlook.com>
2014-05-23 16:51:57 -0500

    database.php config

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'username',
-			'password'  => 'password',
+			'database'  => '',
+			'username'  => '',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
