ec2240d8cb3a284f44415c3095eb2fbce322cee7
Jeff Mills <jeffrey.mills@outlook.com>
2014-05-23 17:43:29 -0500

    New assets

---
+++ app/views/login/index.blade.php
@@ -0,0 +1,12 @@
+@include('includes.head')
+<body>
+	@include('includes.header')
+	@include('includes.alerts')
+	<section>
+		<div class="container">
+		</div>
+	</section>
+	@include('includes.footer')
+	@include('includes.scripts')
+</body>
+</html>
