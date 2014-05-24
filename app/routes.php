95e81969608be9ea260db4c7d0d302eab6b00b9e
Jeff Mills <jeffrey.mills@outlook.com>
2014-05-23 21:09:59 -0500

    Added route group

--- app/routes.php
+++ app/routes.php
@@ -6,3 +6,7 @@ Route::get('/login','LoginController@index');
 Route::post('/login','LoginController@login');
 
 Route::get('/logout','LoginController@logout');
+
+Route::group(['before' => 'auth'], function() {
+	// Protected routes
+});
