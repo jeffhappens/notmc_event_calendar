713b8ae0adb65a3cc89a47b4f7696e9171268d4e
Jeff Mills <jeffrey.mills@outlook.com>
2014-05-23 21:14:36 -0500

    Added route groups and filters

--- app/routes.php
+++ app/routes.php
@@ -9,4 +9,26 @@ Route::get('/logout','LoginController@logout');
 
 Route::group(['before' => 'auth'], function() {
 	// Protected routes
+	Route::group(['before' => 'isAdmin'], function() {
+		// Admin routes
+	});
+
+	Route::group(['before' => 'isClient', function() {
+		// Client routes
+	});
+
+});
+
+
+
+Route::filter('isAdmin', function() {
+	if(Auth::user()->role > 64) {
+		return Redirect::to('/');
+	}
+});
+
+Route::filter('isClient', function() {
+	if(Auth::user()->role > 32) {
+		return Redirect::to('/');
+	}
 });
