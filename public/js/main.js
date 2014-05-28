d2b75a168e5d96ca3e8976fd85d8914d0ec7933c
Jeff Mills <jeffrey.mills@outlook.com>
2014-05-28 09:51:37 -0500

    Removed some JavaScript

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,3 @@
 $(function() {
-	$.ajax({
-		type: 'POST',
-		url: '/admin',
-		dataType: 'JSON',
-		success: function(data) {
-			console.log(data);
-		}
-	})
-
-	$('img[data-error]').error(function() {
-		var error = $(this).data('error');
-		if(this.src !== error) {
-			this.src = error;
-		}
-	});
-
-
+	
 })
